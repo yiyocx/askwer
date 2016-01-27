65189a9c3bb1c0ec00045f8b0a67bdba4cac9f25
Yiyo Castillo <yiyocx@gmail.com>
2016-01-27 17:55:00 -0500

    Added support for React.js
    Added react.js library and configurations for brunch. Tested with a HelloWorld component in React

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,17 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloWorld extends React.Component {
+  render() {    
+    return (<h1>Hello world</h1>);
+  }
+}
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementsByTagName('main')[0]
+)
