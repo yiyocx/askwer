622ae64037c6d73fde675ea052cb7bdc1809e521
Yiyo Castillo <yiyocx@gmail.com>
2016-02-01 09:41:25 -0500

    Created session controller and helper to login user

---
+++ web/controllers/api/session_controller.ex
@@ -0,0 +1,19 @@
+defmodule Askwer.SessionController do
+  use Askwer.Web, :controller
+
+  plug :scrub_params, "session" when action in [:create]
+
+  def create(conn, %{"session" => session_params}) do
+    case Askwer.Session.authenticate(session_params) do
+      {:ok, user} ->
+        Guardian.Plug.sign_in(conn, user)
+        conn
+        |> put_status(:created)
+        |> render(Askwer.RegistrationView, "show.json", user: user)
+      :error ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(Askwer.RegistrationView, "error.json")
+    end
+  end
+end
