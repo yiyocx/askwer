c2d2d116e11106a5b36917aa2010827265acf341
Yiyo Castillo <yiyocx@gmail.com>
2016-01-31 19:33:49 -0500

    Added method to check status of response and throw exceptions

--- web/static/js/utils/utils.js
+++ web/static/js/utils/utils.js
@@ -6,9 +6,20 @@ const defaultHeaders = {
   'Content-Type': 'application/json',
 }
 
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    let error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
 export function httpPost(url, data) {
   const body = JSON.stringify(data);
 
   return fetch(url, {method: 'post', headers: defaultHeaders, body: body})
+    .then(checkStatus)
     .then((response) => response.json());
 };
