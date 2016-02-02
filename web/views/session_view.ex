a48e17266c0f8bd7e4845db4c786f1c8807dcef9
Yiyo Castillo <yiyocx@gmail.com>
2016-02-01 23:59:06 -0500

    Change single quotes to double quotes
    because a single quote-string represents a byte and not text

--- web/views/session_view.ex
+++ web/views/session_view.ex
@@ -6,6 +6,6 @@ defmodule Askwer.SessionView do
   and returns as an array instead of an object to show them in the frontend side
   """
   def render("error.json", _) do
-    %{error: 'Invalid email or password'}
+    %{error: "Invalid email or password"}
   end
 end
